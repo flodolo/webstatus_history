03ed9df43778d25f522d14d28cf9ffb1547ab571
Francesco Lodolo (:flod) <flod@lodolo.net>
2015-12-23 20:05:52 +0100

    Improve delete project script

--- scripts/db_delete_project.py
+++ scripts/db_delete_project.py
@@ -2,12 +2,17 @@
 
 # This script can be used to merge multiple projects into one
 
+import argparse
 import os
 import sqlite3
 import sys
 
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("project_id", help="Project to remove")
+    args = parser.parse_args()
+
     # Get absolute path of ../db from current script location (not current
     # folder)
     db_folder = os.path.abspath(
@@ -23,14 +28,19 @@ def main():
     connection.row_factory = sqlite3.Row
     cursor = connection.cursor()
 
-    project_id = "zamboni"
+    # Check if the project exists
+    cursor.execute("SELECT * FROM stats WHERE project_id=?", [args.project_id])
+    data=cursor.fetchall()
+    if not data:
+        print "Project '%s' is not available" % args.project_id
+        sys.exit(0)
 
     # Delete rows from the stats table
-    cursor.execute("DELETE FROM stats WHERE project_id='%s'" % project_id)
+    cursor.execute("DELETE FROM stats WHERE project_id=?", [args.project_id])
     print "Deleted rows from stats: %s" % cursor.rowcount
 
     # Delete the project
-    cursor.execute("DELETE FROM projects WHERE project_id='%s'" % project_id)
+    cursor.execute("DELETE FROM projects WHERE project_id=?", [args.project_id])
     print "Deleted rows from projects: %s" % cursor.rowcount
 
     # Clean up and close connection
